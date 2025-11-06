227a439351a29352fab4d3d1d55c110cd4a78427
DHARUNKUMAR <dharunkumarsk04@gmail.com>
2025-11-06 19:20:22 +0530

    new changes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json({ status: 'ok', message: 'Hi from sample-app by Dharundk' });
+  res.json({ status: 'ok', message: 'Hi from sample-app by Dharunsk' });
 });
 
 module.exports = app;
